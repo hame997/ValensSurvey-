b2db4ed305518321a460715d5522b9a9376aea17
hame997 <muhamed.ajanovic@edu.fit.ba>
2022-10-23 22:20:38 +0200

    - Test Commit - added user and enum Roles

---
+++ ValensSurveyAPI/ValensSurvey/Enums/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Valens_Survey_Management_API.Enums
+{
+    public enum Roles
+    {
+        Admin = 1,
+        Default
+    }
+}
---
+++ ValensSurveyAPI/ValensSurvey/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Valens_Survey_Management_API.Enums;
+
+namespace ValensSurvey.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+
+    }
+}
